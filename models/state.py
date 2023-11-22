aa9d1cd4b6ee4a2d9bcb25e714416065153688c6
elbatouri rabi <elbatouri@gmail.com>
2023-11-22 12:25:01 -0800

    update base model and init file to switch between storage type

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy.orm import relationship
 
+class State(BaseModel, Base):
+    """State class"""
+    __tablename__ = 'states'
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        @property
+        def cities(self):
+            """Getter attribute that returns the list of City instances"""
+            from models import storage
+            from models.city import City
+
+            city_list = []
+            city_dict = storage.all(City)
+
+            for city in city_dict.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
