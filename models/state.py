495b7dd0839331b509a912e79d9709b2c50b7327
elbatouri rabi <elbatouri@gmail.com>
2023-11-22 19:12:51 -0800

    task 9

--- models/state.py
+++ models/state.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" holds class State"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.city import City
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-class State(BaseModel, Base):
-    """State class"""
-    __tablename__ = 'states'
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+class State(BaseModel, Base):
+    """Representation of state """
+    if getenv('HBNB_TYPE_STORAGE')  == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='all, delete', backref='state')
+        cities = relationship("City", backref="state")
     else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != "db":
         @property
         def cities(self):
-            """Getter attribute that returns the list of City instances"""
-            from models import storage
-            from models.city import City
-
+            """getter for list of city instances related to the state"""
             city_list = []
-            city_dict = storage.all(City)
-
-            for city in city_dict.values():
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
--- models/user.py
+++ models/user.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, String, ForeignKey
-from models.place import Place
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """Representation of a user """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete")
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
