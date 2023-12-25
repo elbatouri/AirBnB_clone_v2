6a4bd2cd4cfaebd0377d297e9c877743a5d3a7ab
elbatouri rabi <elbatouri@gmail.com>
2023-12-25 06:26:29 -0800

    script is number if int

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """return welcoming sentence"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ return welcoming message"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """display 'C '+ text passed with to remove '_' sign"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def Python(text='is_cool'):
+    """display 'Python'+ text passed with to remove '_' sign"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_is_nubmber(n):
+    """return  'n is number'if 'n' integer"""
+    return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
