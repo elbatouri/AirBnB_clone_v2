8030ed41409475b461a8777e1dbafb4db173b392
elbatouri rabi <elbatouri@gmail.com>
2023-12-30 14:43:34 -0800

    make the files executables

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Return a ordred list of states to display it on html page """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
