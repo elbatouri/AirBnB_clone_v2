373476ee8919bc949293dfb06e12adfe6294f075
elbatouri rabi <elbatouri@gmail.com>
2024-01-01 15:25:37 -0800

    fix pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Starts a flask app
     listens to 0.0.0.0:5000
-
 """
+
 from models import storage
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
@@ -15,12 +16,15 @@ def states_list():
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
+
 @app.teardown_appcontext
 def teardown(exc):
     """
     remove the open sqlalchemy session.
     """
-    storage.close()
+
+
+storage.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,21 +9,25 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Return list of states of states
     and related cities ordered by name
-    display it on html page 
+    display it on html page
     """
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
+
 
 @app.teardown_appcontext
 def teardown(exc):
     """
     remove the open sqlalchemy session.
     """
-    storage.close()
+
+
+storage.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
