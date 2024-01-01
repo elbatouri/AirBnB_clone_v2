c4aa854bffae5cf4d1592ff288e02039890ee57a
elbatouri rabi <elbatouri@gmail.com>
2024-01-01 15:17:19 -0800

    make the files excutable

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Return list of states of states
+    and related cities ordered by name
+    display it on html page 
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """
+    remove the open sqlalchemy session.
+    """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
