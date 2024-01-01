6dff6f8592dce9bbb8b1ac07b6ae8364ad8d36dd
elbatouri rabi <elbatouri@gmail.com>
2024-01-01 05:43:24 -0800

    add to the state list script the appteardown method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,5 +12,15 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Return a ordred list of states to display it on html page """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """
+    remove the open sqlalchemy session.
+    """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
