e639e9716448bd459d72fd4958515483b3a66319
elbatouri rabi <elbatouri@gmail.com>
2024-01-03 16:49:17 -0800

    push html template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""Starts a flask app
-    listens to 0.0.0.0:5000
-"""
+"""Starts a Flask web application"""
 
 from models import storage
+from models.state import State
 from flask import Flask
 from flask import render_template
-
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Return a ordred list of states to display it on html page """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+def states():
+    """Returns a rendered html template
+    at the /states_list route,
+    listing all states"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """
-    remove the open sqlalchemy session.
-    """
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
 
-
-storage.close()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
